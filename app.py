025fade0a73c315f3c70618974aa713ce0491d74
averina <you@example.com>
2020-04-21 18:16:22 +0300

    Добавлена база данных и работа с ней

---
+++ app.py
@@ -0,0 +1,9 @@
+from habits import Habits
+
+data = Habits()
+data.add({"name": "Зарядка",
+	 "start": "10.04.2020",
+     "user": "admin",
+	 "end": "14.04.2020",
+	 "chek": "11.04.2020"
+	})
--- habits.py
+++ habits.py
@@ -1,27 +1,29 @@
 import json
+import pymongo
+from pymongo import MongoClient
+import datetime
 
-class Habits(object):
-'''
-Класс содержит в себе 
-список всех привычек и работу с ними.
-
-'''
-
-  def __init__(self):
-	with open("data/habits.json") as file:
-            data = json.load(file)  
-
-	self.habits = data['habits']
-          
-  def add_habit(self, el):
-	self.habits.append(el)
-	
-  def save_habits(self):
-	with open("data/habits.json") as f:
-                pk.dump(self.habits, f)
+class Habits():
     
-  def del_habit(self, name):
-	print("Del ", name)
-
-  def update_habit(self, name):
-	print("Update ", name)
+    def __init__(self):
+        
+        self.client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0-2quke.mongodb.net/test?retryWrites=true&w=majority")
+        self.habits = self.client.habits.habits
+        print(self.habits)
+    
+              
+    def add(self, el):
+        self.habits.insert(el)
+        
+    def get(self, name_habit):
+        return self.habits.find({'name': name_habit})  
+    	
+    def save(self):
+        self.habits.save()
+    
+    #по id? db.bios.find( { _id: 5 } )    
+    def delet(self, id_habit):
+        self.habits.delete_one({ '_id': id_habit })
+    
+    def update(self, last, new):
+        self.habits.update_many(last, new)
