a3e7e2c7af17441867369bc808d8e00b48fed01e
averina <you@example.com>
2020-06-04 14:19:55 +0300

    Ирины хотелки номер два!

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-from app import views
---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+from app.DB.habits import Habits
+from app.DB.persons import Persons
+from datetime import datetime, timedelta
+from flask import Flask	
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+db = Habits()
+@app.route('/')
+@app.route('/index')
+def main():
+	now = datetime.utcnow() + timedelta(hours = 3)
+	weekend = datetime.strptime('2020-06-03 23:59:59', '%Y-%m-%d %H:%M:%S')
+	weekend_minutes_count = (weekend - now).days * 24 * 60 + int((weekend - now).seconds / 60)
+	return '''
+		<div style="font-size:150pz">До четверга осталось {weekend_minutes_count} минут. Держимся!</div>
+	'''.format(weekend_minutes_count=weekend_minutes_count)
