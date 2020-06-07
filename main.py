ddaa1b786e63e0b29d478bc37bb7fa239c8b835d
m.kuchma <athenanebula@gmail.com>
2020-06-07 22:11:57 +0300

    another test icon

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from flask import Flask	
 from datetime import datetime, timedelta
 
-#UPLOAD_FOLDER = '/react_traker/public'
-
-#app = Flask(__name__)
 app = Flask(__name__, static_folder='./react_tracker/public', static_url_path='/')
 app.config['JSON_AS_ASCII'] = False
-#app.config['UPLOAD_FOLDER'] = 'react_tracker/public'
+app.config['UPLOAD_FOLDER'] = 'react_tracker/public'
 
-#def root():
-#    return app.send_static_file("index.html")
 @app.route('/')
-#@app.route('/index')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/react_tracker/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'react_tracker', 'public'),
-                               'favicon.ico', mimetype='image/png')
+@app.route('/loadfile/<filename>')
+def uploaded_file(filename):
+    image = send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return image
