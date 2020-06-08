b335c901a2a4ea9ed386139b27e48f26b2045f30
Ika <ika11ika@yandex.ru>
2020-06-08 12:45:33 +0300

    still testing heroku file system for icon

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask	
 from datetime import datetime, timedelta
 
-app = Flask(__name__, static_folder='./react_tracker/public', static_url_path='/')
+app = Flask(__name__, static_folder='./react_tracker/public/', static_url_path='/')
 app.config['JSON_AS_ASCII'] = False
 app.config['UPLOAD_FOLDER'] = 'react_tracker/public'
 
@@ -9,7 +9,8 @@ app.config['UPLOAD_FOLDER'] = 'react_tracker/public'
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/favicon.ico')
+@app.route('favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, './react_tracker/puplic'),
+    print(str(send_from_directory(os.path.join(app.root_path, './react_tracker/puplic/'),'favicon.ico')))
+    return send_from_directory(os.path.join(app.root_path, './react_tracker/puplic/'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
