e1b21b05ff2123f4a43138645fd9fe52c1b42080
m.kuchma <athenanebula@gmail.com>
2020-06-08 22:36:52 +0300

    fix bugs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__, static_folder='./react_tracker/public/', static_url_path='/')
 app.config['JSON_AS_ASCII'] = False
+app.config['UPLOAD_FOLDER'] = 'react_tracker/public'
 
 @app.route('/')
 def index():
