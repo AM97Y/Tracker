9a5dec5c54db7af2ea7d6b432900fbeb0fb59889
Ika <ika11ika@yandex.ru>
2020-06-08 13:16:10 +0300

    still testing heroku file system for icon

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ def index():
 	'''.format(weekend_minutes_count=str(db.get_all()))
     #return app.send_static_file('index.html')
 
+'''
 @app.route('/favicon.ico')
 def favicon():
     print(str(send_from_directory(os.path.join(app.root_path, './react_tracker/puplic/'),'favicon.ico')))
     return send_from_directory(os.path.join(app.root_path, './react_tracker/puplic/'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+'''
