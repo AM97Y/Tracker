28b69d0d4bf984549280ba3f9ce79f14a0269658
averina <you@example.com>
2020-06-10 14:40:21 +0300

    Bugfix

---
+++ flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+from datetime import datetime, timedelta
+from flask_cors import CORS, cross_origin
+from DB.habits import Habits
+#import DB.parser  
+import os
+
+app = Flask(__name__, static_url_path='/')
+app.config['JSON_AS_ASCII'] = False
+app.config['UPLOAD_FOLDER'] = '/public'
+CORS(app)
+
+@app.route('/')
+@cross_origin(origin='*')
+def index():
+    return ''' <div>Hello</div> '''
