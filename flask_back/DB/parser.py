a6259547b202bddbd8bff5a7d0db0037a78106b1
averina <you@example.com>
2020-06-10 15:06:47 +0300

    ДОбавлены функции

--- flask_back/DB/parser.py
+++ flask_back/DB/parser.py
@@ -1,4 +1,4 @@
-from habits import Habits
+from flask_back.DB.habits import Habits
 
 def get_data():
     db = Habits()
@@ -14,10 +14,10 @@ def get_habit(name_habit):
         return db.get(name_habit=name_habit)
     except:
         return None
+
 def check():
     db = Habits()
     #db.del_habits()
     db.add('Зарядка', 'admin', '1-04-2020', '15-04-2020')
     db.add_check('Зарядка', 'admin', '1-04-2020', '15-04-2020')
-check()
-print(get_data())
+
--- flask_back/__init__.py
+++ flask_back/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from datetime import datetime, timedelta
 from flask_cors import CORS, cross_origin
-from flask_back.DB.habits import Habits
-#import flask_back.DB.parser  
+import flask_back.DB.parser  
 import os
 
 app = Flask(__name__, static_url_path='/')
 app.config['JSON_AS_ASCII'] = False
-app.config['UPLOAD_FOLDER'] = '/public'
 CORS(app)
 
 
@@ -15,4 +13,7 @@ CORS(app)
 @cross_origin(origin='*')
 def index():
     return ''' <div>Hello</div> '''
+
+def get():
+    return flask_back.DB.parser.get_data()
 
