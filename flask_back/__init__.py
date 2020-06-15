498bfdf7252b170796f7e9bfb272a112e7e39584
Ika <ika11ika@yandex.ru>
2020-06-15 21:10:13 +0300

    change return get person data

--- flask_back/__init__.py
+++ flask_back/__init__.py
@@ -24,7 +24,7 @@ def authorization():
 @app.route('/get_person_data')
 @cross_origin(origin='*')
 def get_data(): 
-    return jsonify(flask_back.DB.parser.get_person_data(request.args.get('_id')))
+    return jsonify(list(flask_back.DB.parser.get_person_data(request.args.get('_id'))))
 
 @app.route('/add_person')
 @cross_origin(origin='*')
