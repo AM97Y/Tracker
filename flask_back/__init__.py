42c0565c997fa91eab56dffdf8475318247078eb
Ika <ika11ika@yandex.ru>
2020-06-15 22:19:43 +0300

    still testing front get

--- flask_back/__init__.py
+++ flask_back/__init__.py
@@ -25,7 +25,7 @@ def authorization():
 @cross_origin(origin='*')
 def get_data(): 
     hab = flask_back.DB.parser.get_person_data(request.args.get('_id'))[0].items()
-    return jsonify(hab[0])
+    return jsonify(hab)
     #jsonify({'data': flask_back.DB.parser.get_person_data(request.args.get('_id'))[0]})
 
 @app.route('/add_person')
