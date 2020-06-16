7c9bb84ba767293817ef108654526ab5f2f307fa
averina <you@example.com>
2020-06-16 10:40:40 +0300

    Bugfix.

--- flask_back/__init__.py
+++ flask_back/__init__.py
@@ -27,8 +27,8 @@ def authorization():
 @cross_origin(origin='*')
 def get_data(): 
     habits = (flask_back.DB.parser.get_person_data(request.args.get('_id')))
-    print(habits)
-    json_record2 = bsonjs.dumps(habits)
+    print({'results': habits})
+    json_record2 = bsonjs.dumps({'results': habits})
     print(json_record2)
     return json_record2
     #return jsonify({'results': habs})
